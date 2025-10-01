80894ea7aa1caab33dfa788c07df026ee037e21b
Mariane Andrade <86253405+SpeciousSoup956@users.noreply.github.com>
2025-10-01 15:02:20 -0300

    Calculadora IMC - Aula 04

---
+++ CalculadoraIMC/CalculadoraIMC/Form1.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace CalculadoraIMC
+{
+    partial class calculadoraIMC
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtIdade = new System.Windows.Forms.TextBox();
+            this.txtPeso = new System.Windows.Forms.TextBox();
+            this.txtAltura = new System.Windows.Forms.TextBox();
+            this.lblNome = new System.Windows.Forms.Label();
+            this.lblidade = new System.Windows.Forms.Label();
+            this.lblPeso = new System.Windows.Forms.Label();
+            this.lblAltura = new System.Windows.Forms.Label();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.lblIMC = new System.Windows.Forms.Label();
+            this.lblSituacao = new System.Windows.Forms.Label();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.groupBoxResultado = new System.Windows.Forms.GroupBox();
+            this.groupBoxResultado.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(103, 65);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(100, 20);
+            this.txtNome.TabIndex = 0;
+            // 
+            // txtIdade
+            // 
+            this.txtIdade.Location = new System.Drawing.Point(103, 104);
+            this.txtIdade.Name = "txtIdade";
+            this.txtIdade.Size = new System.Drawing.Size(100, 20);
+            this.txtIdade.TabIndex = 1;
+            this.txtIdade.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtIdade_KeyPress);
+            // 
+            // txtPeso
+            // 
+            this.txtPeso.Location = new System.Drawing.Point(103, 147);
+            this.txtPeso.Name = "txtPeso";
+            this.txtPeso.Size = new System.Drawing.Size(100, 20);
+            this.txtPeso.TabIndex = 2;
+            this.txtPeso.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtPeso_KeyPress);
+            // 
+            // txtAltura
+            // 
+            this.txtAltura.Location = new System.Drawing.Point(103, 187);
+            this.txtAltura.Name = "txtAltura";
+            this.txtAltura.Size = new System.Drawing.Size(100, 20);
+            this.txtAltura.TabIndex = 3;
+            this.txtAltura.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtAltura_KeyPress);
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.Location = new System.Drawing.Point(62, 68);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(38, 13);
+            this.lblNome.TabIndex = 4;
+            this.lblNome.Text = "Nome:";
+            this.lblNome.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblidade
+            // 
+            this.lblidade.AutoSize = true;
+            this.lblidade.Location = new System.Drawing.Point(62, 107);
+            this.lblidade.Name = "lblidade";
+            this.lblidade.Size = new System.Drawing.Size(37, 13);
+            this.lblidade.TabIndex = 5;
+            this.lblidade.Text = "Idade:";
+            // 
+            // lblPeso
+            // 
+            this.lblPeso.AutoSize = true;
+            this.lblPeso.Location = new System.Drawing.Point(62, 150);
+            this.lblPeso.Name = "lblPeso";
+            this.lblPeso.Size = new System.Drawing.Size(34, 13);
+            this.lblPeso.TabIndex = 6;
+            this.lblPeso.Text = "Peso:";
+            // 
+            // lblAltura
+            // 
+            this.lblAltura.AutoSize = true;
+            this.lblAltura.Location = new System.Drawing.Point(62, 190);
+            this.lblAltura.Name = "lblAltura";
+            this.lblAltura.Size = new System.Drawing.Size(37, 13);
+            this.lblAltura.TabIndex = 7;
+            this.lblAltura.Text = "Altura:";
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Location = new System.Drawing.Point(6, 3);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(55, 13);
+            this.lblResultado.TabIndex = 8;
+            this.lblResultado.Text = "Resultado";
+            // 
+            // lblIMC
+            // 
+            this.lblIMC.AutoSize = true;
+            this.lblIMC.Location = new System.Drawing.Point(6, 39);
+            this.lblIMC.Name = "lblIMC";
+            this.lblIMC.Size = new System.Drawing.Size(26, 13);
+            this.lblIMC.TabIndex = 9;
+            this.lblIMC.Text = "IMC";
+            // 
+            // lblSituacao
+            // 
+            this.lblSituacao.AutoSize = true;
+            this.lblSituacao.Location = new System.Drawing.Point(6, 82);
+            this.lblSituacao.Name = "lblSituacao";
+            this.lblSituacao.Size = new System.Drawing.Size(49, 13);
+            this.lblSituacao.TabIndex = 10;
+            this.lblSituacao.Text = "Situação";
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.BackColor = System.Drawing.Color.LightCoral;
+            this.btnCalcular.Location = new System.Drawing.Point(309, 190);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 11;
+            this.btnCalcular.Text = "Calcular IMC";
+            this.btnCalcular.UseVisualStyleBackColor = false;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Clic);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.BackColor = System.Drawing.Color.LightCoral;
+            this.btnLimpar.Location = new System.Drawing.Point(228, 190);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpar.TabIndex = 12;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = false;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // groupBoxResultado
+            // 
+            this.groupBoxResultado.BackColor = System.Drawing.Color.MistyRose;
+            this.groupBoxResultado.Controls.Add(this.lblSituacao);
+            this.groupBoxResultado.Controls.Add(this.lblIMC);
+            this.groupBoxResultado.Controls.Add(this.lblResultado);
+            this.groupBoxResultado.Location = new System.Drawing.Point(228, 65);
+            this.groupBoxResultado.Name = "groupBoxResultado";
+            this.groupBoxResultado.Size = new System.Drawing.Size(156, 102);
+            this.groupBoxResultado.TabIndex = 13;
+            this.groupBoxResultado.TabStop = false;
+            // 
+            // calculadoraIMC
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SeaShell;
+            this.ClientSize = new System.Drawing.Size(469, 262);
+            this.Controls.Add(this.groupBoxResultado);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.lblAltura);
+            this.Controls.Add(this.lblPeso);
+            this.Controls.Add(this.lblidade);
+            this.Controls.Add(this.lblNome);
+            this.Controls.Add(this.txtAltura);
+            this.Controls.Add(this.txtPeso);
+            this.Controls.Add(this.txtIdade);
+            this.Controls.Add(this.txtNome);
+            this.Name = "calculadoraIMC";
+            this.Text = "Calculadora IMC";
+            this.Load += new System.EventHandler(this.calculadoraIMC_Load);
+            this.groupBoxResultado.ResumeLayout(false);
+            this.groupBoxResultado.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtIdade;
+        private System.Windows.Forms.TextBox txtPeso;
+        private System.Windows.Forms.TextBox txtAltura;
+        private System.Windows.Forms.Label lblNome;
+        private System.Windows.Forms.Label lblidade;
+        private System.Windows.Forms.Label lblPeso;
+        private System.Windows.Forms.Label lblAltura;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.Label lblIMC;
+        private System.Windows.Forms.Label lblSituacao;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.GroupBox groupBoxResultado;
+    }
+}
+
---
+++ CalculadoraIMC/CalculadoraIMC/Form1.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculadoraIMC
+{
+    public partial class calculadoraIMC : Form
+    {
+        public calculadoraIMC()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            if (ValidarCampos())
+            {
+                CalcularIMC();
+            }
+
+        }
+        private bool ValidarCampos()
+        {
+            // Validação do nome
+            if (string.IsNullOrWhiteSpace(txtNome.Text))
+            {
+                MessageBox.Show("Por favor, informe o nome.", "Aviso",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtNome.Focus();
+                return false;
+            }
+
+            // Validação da idade
+            if (!int.TryParse(txtIdade.Text, out int idade) || idade <= 0 || idade > 150)
+            {
+                MessageBox.Show("Por favor, informe uma idade válida (1-150).", "Aviso",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtIdade.Focus();
+                return false;
+            }
+
+            // Validação do peso
+            if (!double.TryParse(txtPeso.Text, out double peso) || peso <= 0 || peso > 500)
+            {
+                MessageBox.Show("Por favor, informe um peso válido (ex: 70.5).", "Aviso",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtPeso.Focus();
+                return false;
+            }
+
+            // Validação da altura
+            if (!double.TryParse(txtAltura.Text, out double altura) || altura <= 0 || altura > 3)
+            {
+                MessageBox.Show("Por favor, informe uma altura válida em metros (ex: 1.75).", "Aviso",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtAltura.Focus();
+                return false;
+            }
+
+            return true;
+        }
+        private void CalcularIMC()
+        {
+            double peso = Convert.ToDouble(txtPeso.Text);
+            double altura = Convert.ToDouble(txtAltura.Text);
+
+            // Cálculo do IMC: peso / (altura * altura)
+            double imc = peso / (altura * altura);
+
+            // Exibir resultados
+            lblIMC.Text = $"IMC: {imc:F2}";
+            lblSituacao.Text = $"Situação: {ClassificarIMC(imc)}";
+
+            groupBoxResultado.Visible = true;
+        }
+
+        private string ClassificarIMC(double imc)
+        {
+            if (imc < 18.5)
+                return "Abaixo do peso";
+            else if (imc < 25)
+                return "Peso normal";
+            else if (imc < 30)
+                return "Sobrepeso";
+            else if (imc < 35)
+                return "Obesidade Grau I";
+            else if (imc < 40)
+                return "Obesidade Grau II";
+            else
+                return "Obesidade Grau III";
+        }
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            LimparCampos();
+        }
+
+        private void LimparCampos()
+        {
+            txtNome.Clear();
+            txtIdade.Clear();
+            txtPeso.Clear();
+            txtAltura.Clear();
+            groupBoxResultado.Visible = false;
+            txtNome.Focus();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Configurações iniciais
+            groupBoxResultado.Visible = false;
+        }
+
+        private void calculadoraIMC_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtIdade_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // Permitir apenas números e backspace
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+
+        }
+
+        private void txtPeso_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // Permitir números, vírgula e backspace
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && e.KeyChar != ',')
+            {
+                e.Handled = true;
+            }
+
+            // Permitir apenas uma vírgula
+            if (e.KeyChar == ',' && ((TextBox)sender).Text.Contains(","))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void txtAltura_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // Permitir números, vírgula e backspace
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && e.KeyChar != ',')
+            {
+                e.Handled = true;
+            }
+
+            // Permitir apenas uma vírgula
+            if (e.KeyChar == ',' && ((TextBox)sender).Text.Contains(","))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void btnCalcular_Clic(object sender, EventArgs e)
+        {
+
+            if (ValidarCampos())
+            {
+                CalcularIMC();
+            }
+
+        }
+    }
+}
+
+    
+
